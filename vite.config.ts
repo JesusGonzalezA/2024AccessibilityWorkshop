10d1d4310ebddb7553e6cf110ed2548c7dae07d6
Jesús González Álvarez <Jesus.Gonzalez@unit4.com>
2024-07-26 08:54:49 +0200

    Install storybook

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import mkcert from'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    https: true
-  },
   plugins: [mkcert({
     savePath: './certs'
   }), react()],
