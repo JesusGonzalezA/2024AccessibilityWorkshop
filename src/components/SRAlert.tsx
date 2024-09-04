1dc7ca6c776f0d4e6045d73e46718782108ffe9d
Jesús González Álvarez <Jesus.Gonzalez@unit4.com>
2024-09-04 09:37:33 +0200

    Fix alerts

--- src/components/SRAlert.tsx
+++ src/components/SRAlert.tsx
@@ -1,15 +1,23 @@
 import { useSelector } from "react-redux"
 import styles from "./srAlert.module.css"
-import { lastAlertMessage, lastPoliteMessage } from "../state/srAlertReducer"
+import { allAlertMessages, allPoliteMessages } from "../state/srAlertReducer"
 
 export const SRAlert = () => {
-  const politeMsg = useSelector(lastPoliteMessage)
-  const alertMsg = useSelector(lastAlertMessage)
+  const alertMsgs = useSelector(allAlertMessages)
+  const politeMsgs = useSelector(allPoliteMessages)
 
   return (
     <div className={styles.srOnly}>
-      <div aria-live="polite">{politeMsg}</div>
-      <div aria-live="assertive">{alertMsg}</div>
+      <div aria-live="polite">
+        {politeMsgs.map((m) => (
+          <div>{m}</div>
+        ))}
+      </div>
+      <div aria-live="assertive">
+        {alertMsgs.map((m) => (
+          <div>{m}</div>
+        ))}
+      </div>
     </div>
   )
 }
