99d0a464e7e1f3e7ff5b1520e4df51e8ef3b998d
Jesús González Álvarez <Jesus.Gonzalez@unit4.com>
2024-09-04 10:55:49 +0200

    Fix automatic issue

--- src/components/InputNumber.tsx
+++ src/components/InputNumber.tsx
@@ -6,6 +6,7 @@ import DeleteIcon from "@mui/icons-material/Delete"
 type Props = AllHTMLAttributes<HTMLInputElement> & {
   productTitle: string
   inputLabel: string
+  inputSRLabel: string
   inputId: string
   onRemove: () => void
   onEdit: (arg: number) => void
@@ -20,6 +21,7 @@ export const InputNumber = forwardRef(
       productTitle,
       inputId,
       inputLabel,
+      inputSRLabel,
       onRemove,
       onEdit,
     }: Props,
@@ -44,7 +46,13 @@ export const InputNumber = forwardRef(
         spacing={2}
         className={styles.container}
       >
-        <p className={styles.label}>{inputLabel}</p>
+        <label
+          htmlFor={inputId}
+          aria-label={inputSRLabel}
+          className={styles.label}
+        >
+          {inputLabel}
+        </label>
         <input
           id={inputId}
           value={value}
